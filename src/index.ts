c3d33e7e424a022d4bd8736c0f966b8dccca06d9
Stephen DuVall <stephen.duvall13@gmail.com>
2019-08-30 10:16:22 -0400

    Just commiting this code. It doesn't actually work tho

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import net from 'net';
+
+const options = {
+  hostname: 'mc.stephenduvall.me',
+  port: 25565,
+}
+
+interface Status {
+  ping?: number,
+  version?: string,
+  motd?: string,
+  players?: number,
+  max_players?: number,
+}
+
+interface McServer {
+  ip: string
+  port?: number
+}
+
+let res: Status = {} // Need to figure how to assign vars at different times
+
+function checkStatus(server: McServer): Status {
+
+
+  return res
+}
+
+const start_time = new Date()
+const client = net.connect(options.port, options.hostname, () => {
+
+  res.ping = Math.round(new Date().getMilliseconds() - start_time.getMilliseconds())
+
+  let buff = Buffer.from([ 0xFE, 0x01 ])
+  client.write(buff)
+  console.log(`Ping: ${res.ping}`)
+})
+
+let data: string[] = []
+
+client.on('data', (d) => {
+  // process.stdout.write(d)
+  data.push(d.toString())
+})
+
+client.on('end', () => {
+  let server_info = data.toString().split('\x00\x00\x00')
+
+  server_info.map(d => {
+    d.toString().replace(/\u0000/g, '')
+    console.log(d)
+  })
+})
