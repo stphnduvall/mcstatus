bed69d7692ca0cc1a819c3b465d203730097afc8
Stephen DuVall <stephen.duvall13@gmail.com>
2019-09-06 11:06:26 -0400

    Changed the res object to be declared and initalized with data at the same time

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,6 @@ const options: McServer= {
   port: 25565,
 }
 
-let res: Status = {} // Need to figure how to assign vars at different times
-
 export default function checkStatus(server: McServer) {
   const start_time = new Date()
   let ping: Number
@@ -28,9 +26,12 @@ export default function checkStatus(server: McServer) {
   client.on('end', () => {
     let server_info = data.split('\x00\x00\x00')
 
-    res.version = server_info[2].replace(/\u0000/g, '')
-    res.motd = server_info[3].replace(/\u0000/g, '')
-    res.players = Number(server_info[4].replace(/\u0000/g, ''))
-    res.max_players = Number(server_info[5].replace(/\u0000/g, ''))
+    let res: Status = {
+      ping: Number(ping),
+      version: server_info[2].replace(/\u0000/g, ''),
+      motd: server_info[3].replace(/\u0000/g, ''),
+      players: Number(server_info[4].replace(/\u0000/g, '')),
+      max_players: Number(server_info[5].replace(/\u0000/g, '')),
+    }
   })
 }
